89923aca4609cf000f33c0fd6335e055fd8c24a6
+ <+>
2022-02-28 21:07:23 -0500

    commit tree 23c20dab0051dd4ab8151a417b6f2cc9b31d9567

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const animalSounds = {moo: 'cow', neigh:'horse', baa:'sheep', oink:'pig', cluck:'chicken'}
+const {moo, neigh, baa, oink, cluck} = animalSounds
+const animalNames = {bessie: 'cow', dolly:'sheep', babe:'pig', little:'chicken'}
+const {bessie, dolly, babe, little} = animalNames
+
+  const diffColors = {blackAndWhite: 'cow', black:'sheep', pink:'pig'}
+  const {blackAndWhite, black, pink} = diffColors
+
+  const rainbowColors = { red: 'red', orange:'orange', yellow:'yellow', green:'green', blue:'blue', indigo:'indigo', violet:'violet'}
+  const {red, orange, yellow, green, blue, indigo, violet} = rainbowColors
+
+  const rainbowIntital = {r: 'red', o: 'orange', y:'yellow', g: 'green', b:'blue', indg:'indigo', v:'violet'}
+  const { r, o , y , g, b , indg, v} = rainbowIntital 
+  
+  const {muppetName, color, song, job, partner} = muppet
+  const kermitSong = {song1: 'Rainbow Connection',
+  song2: 'Moving Right Along',
+  song3: 'Never Before, Never Again',
+  song4: 'I Hope That Something Better Comes Along',
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
+}
+const {song2, song4, nestedJob, nestedPartner} = kermitSong
+
 
 // Strings
 
